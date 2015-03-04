16ae625984cf60fff7a8c5c0d8982a0f726549a6
Frédéric Plante <frederic.plante.76@gmail.com>
2015-03-03 21:30:19 -0500

    Update DRV8830_Accélération.lua

--- DRV8830_Accélération.lua
+++ DRV8830_Accélération.lua
@@ -2,12 +2,16 @@ require("DRV8830")
 
 DRV8830.init(3, 4, 0x68)
 
+print("Acceleration positive et vitesse en augmentation \n")
+
 for x = 10 ,30, 1 do
 
 DRV8830.appliquer_vitesse(x)
 tmr.delay(1000000)
 
 end
+
+print("Acceleration negative et vitesse en diminution \n")
 
 for x = 30 ,10, -1 do
 
@@ -16,12 +20,16 @@ tmr.delay(1000000)
 
 end
 
+print("Acceleration negative et vitesse en augmentation \n")
+
 for x = -10 ,-30, -1 do
 
 DRV8830.appliquer_vitesse(x)
 tmr.delay(1000000)
 
 end
+
+print("Acceleration positive et vitesse en diminution \n")
 
 for x = -30 ,-10, 1 do
 
@@ -31,6 +39,11 @@ tmr.delay(1000000)
 end
 
 DRV8830.appliquer_vitesse(0)
+
+print("Acceleration et vitesse nule \n")
+
+print("Demonstration terminee")
+
 
 DRV8830 = nil
 package.loaded["DRV8830"]=nil
